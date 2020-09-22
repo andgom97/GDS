8fd910d26d18184a5abef8ba28d8036827084196
andgom97 <andgomalf@gmail.com>
2020-09-22 19:11:49 +0200

    Functions to make an http request and get the game price

---
+++ src/URLs.py
@@ -0,0 +1,12 @@
+PSSTR = 'https://store.playstation.com'
+PRODUCT = PSSTR + '/es-es/product/'
+GAMES = {'read-dead-redemption-2':'EP1004-CUSA08519_00-REDEMPTION000002',
+         'spyro':'EP0002-CUSA12085_00-SPYROTRILOGY0001',
+         'sekiro':'EP0002-CUSA13801_00-SEKIROGAME000001',
+         'god-of-war':'EP9000-CUSA07411_00-0000000GODOFWARN',
+         'dead-cells':'EP3862-CUSA10484_00-DEADCELLS0000000',
+         'nier-automata':'EP0082-CUSA04480_00-GOTYORHADIGITAL0',
+         'doom':'EP1003-CUSA02092_00-DOOMEUROPEROWSKU',
+         'doom-eternal':'EP1003-CUSA13275_00-DOOMETERNALBUNDL',
+         'dishonored-2':'EP1003-CUSA04806_00-DISHONOREDTWOPS4'
+        }
---
+++ src/scrapper.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from URLs import *
+from prcolors import *
+
+# Function that returns the object from the http request
+def get_resource(url):
+    # Http request to https://store.playstation.com
+    try:
+        page = requests.get(url)
+        # If not 200 return None
+        if not page.ok:
+            prRed(f"Error getting resource: {url}")
+            prLightOrange(f"Response code: {page}")
+            return None
+        return page
+    except Exception as e:
+        prRed('Error getting resource')
+        prLightOrange(e)
+        return None
+
+# Function that gets the price of a game
+def get_game_price(url):
+    page = get_resource(url)
+    try:
+        soup = BeautifulSoup(page.content,'html.parser')
+        game_price = soup.find('h3',class_='price-display__price')
+        return game_price.text[:-1] 
+    except AttributeError:
+        return None
